31b74f9cc464b0108b3e8ead7ec1eec50fc70276
nicollekoetzler <nickkoetzler@gmail.com>
2022-09-30 00:00:27 -0300

    fix: send the list

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -5,7 +5,7 @@ export async function getCategories (req, res) {
         const {rows: categories} = await connection.query('SELECT * FROM categories;');
 
         console.log(categories);
-        res.sendStatus(503);
+        res.status(503).send(categories);
     }catch(err){
         console.log(err)
         res.sendStatus(500);
