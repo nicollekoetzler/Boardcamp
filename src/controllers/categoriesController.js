0b713f8dc2bdf55f71c399bcd03b314748b671f7
nicollekoetzler <nickkoetzler@gmail.com>
2022-10-01 15:49:19 -0300

    feat: create new categories in /categories and validate body

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,17 +1,39 @@
-import connection from "../databases/database.js"
+import connection from '../databases/database.js';
+import joi from 'joi';
 
 export async function getCategories (req, res) {
     try {
         const {rows: categories} = await connection.query('SELECT * FROM categories;');
 
-        console.log(categories);
         res.status(503).send(categories);
     }catch(err){
-        console.log(err)
+        console.log(err);
         res.sendStatus(500);
     }
 }
 
 export async function createCategories (req, res) {
-    
+    try {
+        const category = req.body;
+
+        const categorySchema = joi.object({
+            name: joi.string().required()
+        });
+
+
+        const isBodyValid = categorySchema.validate(category);
+        
+        if ( isBodyValid.error ){
+            return res.sendStatus(422);
+        }
+
+        //TODO validar se nome da categoria já existe
+
+        await connection.query('INSERT INTO categories (name) VALUES ($1);', [ category.name] );
+
+        res.sendStatus(201);
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
